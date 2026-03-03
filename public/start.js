ef8077a217f12872f8d7b03ee02e80eb0f6256e8
LovisaSkoogh <sklo23zz@student.ju.se>
2026-03-03 12:28:27 +0100

    start.js

---
+++ public/start.js
@@ -0,0 +1,23 @@
+// Fetch the stores
+// Displays stores in the console
+fetch("/stores")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Stores loaded:", data);
+
+    // Displays stores on the webpage
+    const list = document.getElementById("storelist");
+    data.forEach((store) => {
+      const li = document.createElement("li");
+      li.innerText = `${store.name}`;
+      list.appendChild(li);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching stores:", error);
+  });
