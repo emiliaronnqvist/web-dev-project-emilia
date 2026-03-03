3ce74b565f9746fe524bd2cf8af9e50152b9deb8
emiliaronnqvist <emilia.ronnqvist@hotmail.com>
2026-03-03 12:53:33 +0100

    .

--- public/start.js
+++ public/start.js
@@ -1,6 +1,6 @@
 // Fetch the stores
 // Displays stores in the console
-/*fetch("/api/stores")
+fetch("/api/stores")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
@@ -21,19 +21,3 @@
   .catch((error) => {
     console.error("Error fetching stores:", error);
   });
-*/
-function fetchStores() {
-  fetch("/stores.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not OK");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Stores:", data);
-
-      // create list
-    });
-}
-fetchStores();
