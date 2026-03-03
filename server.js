71af1e4f7059cd9fad16da4d7f75798dc4c2968a
LovisaSkoogh <sklo23zz@student.ju.se>
2026-03-03 13:00:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3008;
 
+const stores = require("./stores.json");
+
 app.get("/", (req, res) => {
   res.send("Welcome to the REST API!");
 });
