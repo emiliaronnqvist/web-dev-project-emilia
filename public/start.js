3f2a79a5a1279dcb63c809a70fc5183af88b92f4
emiliaronnqvist <emilia.ronnqvist@hotmail.com>
2026-03-03 15:06:03 +0100

    .

--- public/start.js
+++ public/start.js
@@ -10,13 +10,16 @@ fetch("/api/stores")
   .then((data) => {
     console.log("Stores loaded:", data);
 
-    // Displays stores on the webpage
+    /*  // Displays stores on the webpage
     const list = document.getElementById("storelist");
     data.forEach((store) => {
       const li = document.createElement("li");
       li.innerText = `${store.name}`;
       list.appendChild(li);
-    });
+    });*/
+    let card_section = document.createElement("section");
+    div.appendChild(card_section);
+    let card_title = document.createElement("h2");
   })
   .catch((error) => {
     console.error("Error fetching stores:", error);
