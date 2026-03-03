807859805da929daab99cfc75bb51071607821f6
emiliaronnqvist <emilia.ronnqvist@hotmail.com>
2026-03-03 15:45:39 +0100

    .

--- public/start.js
+++ public/start.js
@@ -11,15 +11,25 @@ fetch("/api/stores")
     console.log("Stores loaded:", data);
 
     // Displays stores on the webpage
-    const list = document.getElementById("storelist");
+    const ul = document.getElementById("storelist");
     data.forEach((store) => {
       const li = document.createElement("li");
-      li.innerText = `${store.name}`;
-      list.appendChild(li);
-    });
 
-    let card_section = document.createElement("section");
-    let card_title = document.createElement("h2");
+      const h2 = document.createElement("h2");
+      h2.innerText = `${store.name}`;
+
+      const p = document.createElement("p");
+      p.innerText = `${store.district}`;
+
+      const button = document.createElement("button");
+      button.innerText = "Read more";
+
+      li.appendChild(h2);
+      li.appendChild(p);
+      li.appendChild(button);
+
+      ul.appendChild(li);
+    });
   })
   .catch((error) => {
     console.error("Error fetching stores:", error);
